99b255b674aeff7b49c8fc419314847b2f8d2bda
Carson Lauer <lauercarson@gmail.com>
2026-07-11 20:28:02 -0700

    changes

--- apps/blink/src/main.cpp
+++ apps/blink/src/main.cpp
@@ -1,28 +1,36 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 
-#include "common/proto/sats_command.pb.h"
+#include "cobs.hpp"
+
+#include "tusb.h"
+
+#include "proto/sats_command.pb.h"
 #include "pb_encode.h"
 
 #include "slate.hpp"
 
-#include <array>
-
 static Slate gSlate{};
 
 int main() {
-    ssi_protobuf_WordCommand cmd_out = {4, 4};
-    uint8_t buf[10];
-    pb_ostream_t stream = pb_ostream_from_buffer(buf, sizeof(buf));
-    pb_encode(&stream, ssi_protobuf_WordCommand_fields, &cmd_out);
+    stdio_init_all();
+
+    sleep_ms(1000);
 
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    uint8_t buf[64];
     while (true) {
-        gSlate.cycle_count += 1;
+        tud_task();
         gpio_put(LED_PIN, 1);
-        sleep_ms(250);
+        sleep_ms(gSlate.sleep_ms);
         gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        sleep_ms(gSlate.sleep_ms);
+        if (tud_cdc_available()) {
+            uint32_t count = tud_cdc_read(buf, sizeof(buf));
+            tud_cdc_write(buf, count);
+            tud_cdc_write_flush();
+        }
     }
 }
--- apps/blink/src/slate.hpp
+++ apps/blink/src/slate.hpp
@@ -1,5 +1,7 @@
+#pragma once
+
 struct Slate {
-    uint32_t cycle_count{};
+    uint32_t sleep_ms{250};
     float temperature{};
     float voltage{};
     float current{};
